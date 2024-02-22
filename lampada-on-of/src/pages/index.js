6e6bec0f4a62b44f2de6fbd22c936ec48c5910ed
bryanribeiro29 <129908079+bryanribeiro29@users.noreply.github.com>
2024-02-21 22:18:27 -0300

    commit implementação do state

--- lampada-on-of/src/pages/index.js
+++ lampada-on-of/src/pages/index.js
@@ -1,5 +1,33 @@
+import Image from "next/image";
+import { useState } from "react";
+
 export default function Home() {
+  const [estadoInterruptor, setEstadoInterruptor] = useState(false)
+  const [contador, setContador] = useState(0)
+  
+  function mudarLampada() {
+    setEstadoInterruptor(!estadoInterruptor)
+    if (estadoInterruptor == false)
+      setContador(contador+1)
+  }
+
   return (
-    <div>Olá Mundo</div>
+    <div>
+      <h2>
+        {
+          contador <5
+          ? `Você já ligou ${contador} vezes a lâmpada ${contador >=5 ?"Acabou queimando" : ""}`
+          : "Sua lâmpada queimou"
+        }
+      </h2>
+
+      {contador >= 5
+      ? <Image src={"/assets/boom.png"} width={360} height={360} />
+      : estadoInterruptor == true
+          ? <Image src={"/assets/on.png"} width={286} height={388} />
+          : <Image src={"/assets/off.png"} width={286} height={388} />
+      }
+      <button onClick={mudarLampada}>Interruptor</button>
+    </div>
   );
 }
